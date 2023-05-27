d4816eb2a8ebc761c128e8ea08b2656483982960
spirus-dev <43066360+spirus-dev@users.noreply.github.com>
2023-05-28 00:05:10 +0530

    Completed the Fibonacci program
    Finished assignment 1 by completing the Fibonacci program using both iterative and recursive approach

---
+++ Fibonacci/program.js
@@ -0,0 +1,18 @@
+function fibs(n){
+    let f0=0;
+    let f1=1;
+    let outputArray=[f0,f1];
+    for(let i=0;i<n-2;i++){
+        let next=f0+f1;
+        outputArray.push(next);
+        f0=f1;
+        f1=next;
+    }
+    return outputArray;
+}
+
+function fibsRec(n){
+    return n>2 ? [...fibsRec(n-1),fibsRec(n-1)[n-2]+fibsRec(n-2)[n-3]] : (n===1 ? [0] : [0,1]);
+}
+
+console.log(fibsRec(8));
